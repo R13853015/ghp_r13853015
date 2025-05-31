892cdde9adef933ef8ad61e3ee6f97c7d6d68b25
Jessica <jl983421@gmail.com>
2025-06-01 00:26:14 +0800

    my README.md File
    This file has the results of my analysis and tells you what analysis method I used.

---
+++ README.md.m
@@ -0,0 +1,7 @@
+# Dengue Assignment -- How Many Total Cases Reported in 2014?
+
+## Data Analysis Results
+
+- Total cases reported in 2014: **15747**
+
+*Analysis done using R with the `readr` package.*
